99d74ed97a90eef51e5ba60148293eb63b43902b
S518646 <S518646@NW28807.nwmissouri.edu>
2014-04-03 14:39:12 -0500

    I have added a statement-Namitha Kodi

---
+++ src/kodigit/KodiGIT.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kodigit;
+
+/**
+ *
+ * @author S518646
+ */
+public class KodiGIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("I have added a statement-Namitha Kodi");// TODO code application logic here
+    }
+}
